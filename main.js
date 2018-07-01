555cb134ec4c2d09d49616483203fbdf49d8e0d4
Syed Ahsan Ahmed <snahmed1998@gmail.com>
2018-07-01 20:48:45 +0500

    index.ejs added

--- main.js
+++ main.js
@@ -6,7 +6,13 @@ app.set('view engine', 'ejs')
 
 //for html
 function home() {
-    
+
+    app.use(express.static('public')); //inline
+    //displaying html requested page
+    app.get('/', function (req, res) {
+        res.render('index', { Name: null, error: null });
+    })
+
     app.listen(3000, function () {
         console.log('Example app listening on port 3000!')
     })
