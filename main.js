4cb1ec9af5f7740b8eaeb284a122fb9499881649
Syed Ahsan Ahmed <snahmed1998@gmail.com>
2018-07-01 20:33:09 +0500

    modules added

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("ge");
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs')
+
+//for html
+function home() {
+    
+    app.listen(3000, function () {
+        console.log('Example app listening on port 3000!')
+    })
+}
+
+home();
